3726d967fefa3c997aafcb983539d99264e0a892
RobKaya <robkaya@icloud.com>
2024-01-26 15:14:08 +0100

    Added search logic part 1

--- src/main/java/com/wacoco/searchFunctionality/SearchFunctionalityApplication.java
+++ src/main/java/com/wacoco/searchFunctionality/SearchFunctionalityApplication.java
@@ -3,11 +3,42 @@ package com.wacoco.searchFunctionality;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Filter;
+
 @SpringBootApplication
 public class SearchFunctionalityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SearchFunctionalityApplication.class, args);
+
+
+	String mainUrl = "https://www.lens.org/lens/search/patent/list";
+
+	public static void main(String[] args) throws IOException {
+
+		//TODO remove mock code here when ready
+		List<String> flagList = new ArrayList<String>(
+				Arrays.asList("Has Title", "Has Abstract", "Has Legal Events")
+		);
+
+		FilterSettings filterSettings = new FilterSettings("Battery", "null", flagList,"null","null","null","null","null","null","null","null","null", "null");
+
+		System.out.println(filterModifier(filterSettings));
+
 	}
 
+
+	public static String filterModifier(FilterSettings settings) {
+		final String mainUrl = "https://www.lens.org/lens/search/patent/list";
+		String filterResult = "";
+
+		filterResult = mainUrl + settings.getKeyWord() + settings.getFlags(settings.flags);
+
+		return filterResult;
+	}
 }
+
+
